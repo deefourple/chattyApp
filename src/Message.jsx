21ecc9800f2df64a1701a83448ab14f6b22ff5ee
deefourple <davedehghani@gmail.com>
2017-02-06 17:23:30 -0500

    Created rendering of HTML through jsx files for ChattyApp

---
+++ src/Message.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Message extends Component {
+  render() {
+  console.log("Rendering <App/>");
+    return (
+    <div className="message">
+
+    </div>
+      );
+  }
+}
+
+export default Message;
---
+++ src/MessageList.jsx
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+
+class MessageList extends Component {
+  render() {
+    console.log("Rendering <MessageList />");
+    return (
+      <div>
+        <nav>
+          <h1>Chatty</h1>
+        </nav>
+        <div id="message-list">
+          <div className="message">
+            <span className="username">Anonymous1</span>
+            <span className="content">I wont be impressed with technology until I can download food.</span>
+          </div>
+          <div className="message system">
+            Anonymous1 changed their name to nomnom.
+          </div>
+        </div>
+      </div>
+
+    );
+  }
+}
+
+export default MessageList;
