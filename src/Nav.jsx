e7f448d9d3523b751f610eea3b6c0d6417bc3445
David Dehghani <davedehghani@gmail.com>
2017-02-16 20:26:48 -0500

    make stateless

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,15 +1,10 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-class Nav extends Component {
-  render() {
-  console.log("Rendering <Nav/>");
-    return (
-        <nav>
-          <h1>Chattie</h1>
-          <h3>{this.props.count} Users Currently Online</h3>
-        </nav>
-      );
-  }
-}
+const Nav = ({ count }) => (
+  <nav>
+    <h1>Chattie</h1>
+    <h3>{count} Users Currently Online</h3>
+  </nav>
+);
 
 export default Nav;
